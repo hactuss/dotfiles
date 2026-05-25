12abf7724bba32b09e669275c44717ada0367d2f
hactuss <hactuss@proton.me>
2026-05-25 22:46:08 +0200

    modified:   hosts/emerald/configuration.nix

--- hosts/emerald/configuration.nix
+++ hosts/emerald/configuration.nix
@@ -8,7 +8,6 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    inputs.home-manager.nixosModules.default
     #inputs.stylix.nixosModules.stylix
   ];
   steam-mod.enable = true;
