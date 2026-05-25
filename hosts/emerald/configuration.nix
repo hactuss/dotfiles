4b9a60cb7e018a2ef4c2015db4e1f39f6dc85a9b
hactuss <hactuss@proton.me>
2026-05-25 13:30:50 +0200

    modified:   hosts/emerald/configuration.nix

--- hosts/emerald/configuration.nix
+++ hosts/emerald/configuration.nix
@@ -162,8 +162,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-
-  home-manager
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
