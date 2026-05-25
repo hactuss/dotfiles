3c7f9d14f28691b80b3161c0a4094cd81fa6b5ba
hactuss <hactuss@proton.me>
2026-05-25 13:28:15 +0200

    modified:   flake.nix 	modified:   hosts/emerald/configuration.nix

--- hosts/emerald/configuration.nix
+++ hosts/emerald/configuration.nix
@@ -8,7 +8,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    inputs.home-manager.nixosModules.default
+    # inputs.home-manager.nixosModules.default
     inputs.stylix.nixosModules.stylix
  #   ./../../modules/nixos/prism.nix
   ];
