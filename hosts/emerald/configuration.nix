77fad877838a62bc489d479c4e1475fd6d9c9456
hactuss <hactuss@proton.me>
2026-06-10 18:02:12 +0200

    wrappers work now

--- hosts/emerald/configuration.nix
+++ hosts/emerald/configuration.nix
@@ -152,6 +152,26 @@
     awww
     imagemagick
     waybar
+
+
+    (inputs.wrappers.lib.wrapPackage {
+      inherit pkgs;
+      package = pkgs.curl;
+      runtimeInputs = [pkgs.jq];
+      env = {
+        CURL_CA_BUNDLE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+      };
+      flags = {
+        "--silent" = true;
+        "--connect-timeout" = "30";
+      };
+      # Or use args directly for more control:
+      # args = [ "--silent" "--connect-timeout" "30" ];
+      flagSeparator = "="; # Use --flag=value instead of --flag value (default is " ")
+      preHook = ''
+        echo "Making request..." >&2
+      '';
+    })
   ];
     neovim-mod.enable = true;
   programs.kdeconnect.enable = true;
