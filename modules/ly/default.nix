c3ee58ef51ba228958abd0f7e461069fbcaa9516
hactuss <hactuss@proton.me>
2026-07-22 21:04:54 +0200

    ly config

--- modules/ly/default.nix
+++ modules/ly/default.nix
@@ -2,8 +2,8 @@
   services.displayManager.ly = {
     enable = true;
     settings = {
-      animations = "colormix";
-      # animations = "gameoflife";
+      animation = "colormix";
+      #animation = "gameoflife";
       show_tty = true;
       bigclock = "en";
       clear_password = true;
