fef1a9d539edea036360b0b64cf3da4fa6f44ab3
hactuss <hactuss@proton.me>
2026-07-23 23:34:24 +0200

    new waybar + laptop batery

--- hosts/opal/configuration.nix
+++ hosts/opal/configuration.nix
@@ -99,6 +99,7 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
+services.power-profiles-daemon.enable = false;
   hardware.bluetooth.enable = true;
   systemd.targets.sleep.enable = false;
   systemd.targets.suspend.enable = false;
@@ -179,6 +180,38 @@ services.logind.settings.Login = {
   HandleLidSwitch = "poweroff";
   HandleLidSwitchExternalPower = "lock";
   HandleLidSwitchDocked = "ignore";
+};
+services.tlp = {
+  enable = true;
+  settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+    CPU_MIN_PERF_ON_AC = 0;
+    CPU_MAX_PERF_ON_AC = 100;
+    CPU_MIN_PERF_ON_BAT = 0;
+    CPU_MAX_PERF_ON_BAT = 20;
+
+    # Optional helps save long term battery health
+    START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+    STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
+    TLP_DEFAULT_MODE = "BAT";
+    TLP_PERSISTENT_DEFAULT = 1;
+  };
+};
+services.auto-cpufreq.enable = true;
+services.auto-cpufreq.settings = {
+  battery = {
+    governor = "powersave";
+    turbo = "never";
+  };
+  charger = {
+    governor = "performance";
+    turbo = "auto";
+  };
 };
 
 # one of "ignore", "poweroff", "reboot", "halt", "kexec", "suspend", "hibernate", "hybrid-sleep", "suspend-then-hibernate", "lock"
