634972a06c0b34fe4cb1e36dcecf4488a15e4685
hactuss <hactuss@proton.me>
2026-07-23 22:48:43 +0200

    ly config working again

--- modules/ly/default.nix
+++ modules/ly/default.nix
@@ -4,15 +4,15 @@
     settings = {
       #animation = "colormix";
       #animation = "gameoflife";
-      animation = "cmatrix";
+      animation = "matrix";
       show_tty = true;
       bigclock = "en";
       clear_password = true;
-      /*blank_box = true;
+      blank_box = true;
       border_fg = "0x0000FF00";
       box_title = "Welcome, User";
       #colormix_col1 = ;
-      margin_box_h = 1.5;*/ 
+      margin_box_h = 1;
     };
   };
 
