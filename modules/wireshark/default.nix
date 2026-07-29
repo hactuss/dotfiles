ce5de00f566a0681a9b1552ae78cf4b63f09873c
hactuss <hactuss@proton.me>
2026-07-29 23:38:16 +0200

    waybar

--- modules/wireshark/default.nix
+++ modules/wireshark/default.nix
@@ -2,8 +2,6 @@
   environment.systemPackages = with pkgs; [
     wireshark
     wireshark-cli
-    wireshark-qt
   ];
-  programs.wayfire.enable = true;
   programs.wireshark.package = pkgs.wireshark;
 }
