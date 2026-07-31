f169ea57251fccc6a846b507363c216eb4841327
hactuss <hactuss@proton.me>
2026-07-31 22:55:02 +0200

    extrainit

--- hosts/emerald/configuration.nix
+++ hosts/emerald/configuration.nix
@@ -143,7 +143,7 @@
     RUSTICL_ENABLE = "radeonsi";
     RUST_BACKTRACE = 1;
   };
-  # environment.loginShellInit = "pushd /home/hactuss/dotfiles; git pull; popd;";
+  environment.extraInit = "pushd /home/hactuss/dotfiles; git pull --ff-only; popd;";
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     alacritty
