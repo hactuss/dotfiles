1ed924c334b1788ea1d4d6b61630108ba141d492
hactuss <157395485+hactuss@users.noreply.github.com>
2026-07-31 21:02:01 +0200

    Update configuration.nix

--- hosts/opal/configuration.nix
+++ hosts/opal/configuration.nix
@@ -238,7 +238,6 @@
     DEFAULT_EDITOR = "neovim";
     EDITOR = "neovim";
   };
-  # environment.loginShellInit = "pushd /home/hactuss/dotfiles; git pull; popd; exit;";
   environment.systemPackages = with pkgs; [
     brave
     librewolf
