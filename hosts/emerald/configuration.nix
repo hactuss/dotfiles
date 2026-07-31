7d2d614dd842f815a67caed89b25f3e4d7a663f7
hactuss <hactuss@proton.me>
2026-07-31 19:19:48 +0200

    locales

--- hosts/emerald/configuration.nix
+++ hosts/emerald/configuration.nix
@@ -63,17 +63,23 @@
   };
   # Select internationalisation properties.
   i18n = {
+    /*
+      defaultLocale = "de_DE.UTF-8";
+      extraLocaleSettings = {
+        LC_ADDRESS = "de_DE.UTF-8";
+        LC_IDENTIFICATION = "de_DE.UTF-8";
+        LC_MEASUREMENT = "de_DE.UTF-8";
+        LC_MONETARY = "de_DE.UTF-8";
+        LC_NAME = "de_DE.UTF-8";
+        LC_NUMERIC = "de_DE.UTF-8";
+        LC_PAPER = "de_DE.UTF-8";
+        LC_TELEPHONE = "de_DE.UTF-8";
+        LC_TIME = "de_DE.UTF-8";
+      };
+    */
     defaultLocale = "de_DE.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "de_DE.UTF-8";
-      LC_IDENTIFICATION = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-      LC_NAME = "de_DE.UTF-8";
-      LC_NUMERIC = "de_DE.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_TELEPHONE = "de_DE.UTF-8";
-      LC_TIME = "de_DE.UTF-8";
+      LC_ALL = "de_DE.UTF-8";
     };
   };
   # Enable the X11 windowing system.
@@ -220,7 +226,6 @@
       '';
     })
   ];
-  programs.kdeconnect.enable = true;
   #services.asusd
 
   # Some programs need SUID wrappers, can be configured further or are
--- modules/dolphin/default.nix
+++ modules/dolphin/default.nix
@@ -1,9 +1,12 @@
-{pkgs, ...}:{
-environment.systemPackages = with pkgs; [
-kdePackages.dolphin
-kdePackages.qtsvg
-kdePackages.kio # needed since 25.11
-    kdePackages.kio-fuse #to mount remote filesystems via FUSE
-    kdePackages.kio-extras #extra protocols support (sftp, fish and more)
-];
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    kdePackages.dolphin
+    kdePackages.qtsvg
+    kdePackages.kio # needed since 25.11
+    kdePackages.kio-fuse # to mount remote filesystems via FUSE
+    kdePackages.kio-extras # extra protocols support (sftp, fish and more)
+  ];
+  environment.etc."xdg/menus/applications.menu".source =
+    "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+
 }
