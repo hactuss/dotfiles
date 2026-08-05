f208594128bc39993de6e4873b8b09716f1d8ce4
hactuss <hactuss@proton.me>
2026-08-05 13:11:49 +0200

    syncthing fix

--- modules/syncthing/default.nix
+++ modules/syncthing/default.nix
@@ -7,7 +7,6 @@
       folders."deltarune" = {
         enable = true;
         path = "/home/hactuss/.steam/steam/steamapps/compatdata/1671210/pfx/drive_c/users/steamuser/AppData/Local/DELTARUNE";
-        type = "sendrecive";
       };
     };
   };
