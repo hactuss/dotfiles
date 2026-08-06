c209c44c3241b3bce15e6398c738fd98149cdd24
hactuss <hactuss@proton.me>
2026-08-06 19:23:10 +0200

    syncthing config now doesnt destroy folders

--- modules/syncthing/default.nix
+++ modules/syncthing/default.nix
@@ -6,20 +6,22 @@
     openDefaultPorts = true;
     settings = {
       gui.user = "hactuss";
-      folders = {
-        "deltarune" = {
-          enable = true;
-          path = "/home/hactuss/.steam/steam/steamapps/compatdata/1671210/pfx/drive_c/users/steamuser/AppData/Local/DELTARUNE";
-        };
-        "undertale" = {
-          enable = true;
-          path = "~/.config/UNDERTALE";
-        };
-        "Obsidian Vault" = {
-          enable = true;
-          path = "~/Obsidian Vault";
-        };
-      };
+      /*
+        folders = {
+          "deltarune" = {
+            enable = true;
+            path = "/home/hactuss/.steam/steam/steamapps/compatdata/1671210/pfx/drive_c/users/steamuser/AppData/Local/DELTARUNE";
+          };
+          "undertale" = {
+            enable = true;
+            path = "~/.config/UNDERTALE";
+          };
+          "Obsidian Vault" = {
+            enable = true;
+            path = "~/Obsidian Vault";
+          };
+          };
+      */
     };
   };
 }
